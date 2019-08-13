5a47b2d86702c9f8f5e1f41eb9507af40f0d9791
Pratik Bhuite <pbhuite@quinstreet.com>
2019-08-13 16:26:30 +0530

    ''

---
+++ SimpleJavaProject/src/main/java/com/adevguide/java/regex/RegularExpressions.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.adevguide.java.regex;
+
+/**
+ * @author prabhu
+ *
+ */
+public class RegularExpressions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
