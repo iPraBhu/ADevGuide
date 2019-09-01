81b8bd4466fd34f2d7f6b73ab769b0fa7a0de847
pbhuite <pbhuite@L480-PBHUITE.ind1.echo.quinstreet.net>
2019-09-01 13:10:13 +0530

    https://www.adevguide.com/factory-method-design-pattern-java/

---
+++ DesignPatterns/src/main/java/com/adevguide/java/designpatterns/factorymethod/Stock.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.adevguide.java.designpatterns.factorymethod;
+
+/**
+ * @author PraBhu
+ *
+ */
+public abstract class Stock {
+    
+    public abstract void buyShares(int n);
+    public abstract void sellShares(int n);
+    //implemented method
+    public void exchangeInfo() {
+        System.out.println("You are making Transactions at NASDAQ");
+    }
+    
+
+}
