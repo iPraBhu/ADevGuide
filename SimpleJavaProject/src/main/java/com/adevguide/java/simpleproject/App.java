a51e7f7b45ebaffc9d67fa077636da760a1db64e
unknown <pratik.bhuite@gmail.com>
2020-01-15 20:42:44 +0530

    Added Main file details in POM

--- SimpleJavaProject/src/main/java/com/adevguide/java/simpleproject/App.java
+++ SimpleJavaProject/src/main/java/com/adevguide/java/simpleproject/App.java
@@ -1,18 +1,9 @@
 package com.adevguide.java.simpleproject;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class App {
     public static void main(String[] args) {
-String s="<?xml version='1.0' encoding='utf-8'?><soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSc\r\n" + 
-        "hema-instance\"><soap:Body><PostLead xmlns=\"http://ws.fahw.com/\"><lead>&lt;FahbpLead>&lt;FirstName>Sam&lt;/FirstName>&lt;LastName>Dave&lt;/LastName>&lt;Address>sam daejf&lt;/Address>&lt;City>GLENDALE&lt;/City>&lt;State>85305&lt;/State>&lt;Zip>85305&lt\r\n" + 
-        ";/Zip>&lt;Phone>8082957338&lt;/Phone>&lt;EmailAddress>gpgffhawlah11@icloud.com&lt;/EmailAddress>&lt;SourceOfOrder>BM232&lt;/SourceOfOrder>&lt;PropertyType>&lt;/PropertyType>&lt;AltPhone>&lt;/AltPhone >&lt;SourceID>20647026023&lt;/SourceID >&lt;SubVen\r\n" + 
-        "dor>&lt;/SubVendor>&lt;ScheduledApptDT>&lt;/ScheduledApptDT>&lt;/FahbpLead></lead></PostLead></soap:Body></soap:Envelope>";
-
-String requestObject=s.replaceAll("&lt;","<");
-
-System.out.println(requestObject);
-
+    System.out.println("\n\n***************************************\n\n");
+    System.out.println("This Application is build using Docker.");
+    System.out.println("\n\n***************************************\n\n");
     }
 }
