13a14f4fe78b0fac74528e0d302767f1a3a1949a
unknown <pratik.bhuite@gmail.com>
2020-01-20 21:25:32 +0530

    main file modified

--- SimpleJavaProject/src/main/java/com/adevguide/java/simpleproject/App.java
+++ SimpleJavaProject/src/main/java/com/adevguide/java/simpleproject/App.java
@@ -1,9 +1,15 @@
 package com.adevguide.java.simpleproject;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
     System.out.println("\n\n***************************************\n\n");
     System.out.println("This Application is build using Docker.");
-    System.out.println("\n\n***************************************\n\n");
+    System.out.println("Type Something and it will be displayed on the console.");
+    Scanner in=new Scanner(System.in);
+    String input=in.nextLine();
+    System.out.println("You have entered :: "+input);
+    System.out.println("\n\nOk Bye********************************\n\n");
     }
 }
