18f976bad87981346e0d4733fabe94cf985e2102
unknown <pratik.bhuite@gmail.com>
2020-01-29 22:52:22 +0530

    cd 'c:\Users\PraBhu\gitProjects\ADevGuide\SimpleJavaProject'; & 'C:\Users\PraBhu\.vscode\extensions\vscjava.vscode-java-debug-0.24.0\scripts\launcher.bat' 'C:\Program Files\Java\jdk1.8.0_66\bin\java' '-Dfile.encoding=UTF-8' '-cp' 'C:\Users\PraBhu\gitProjects\ADevGuide\SimpleJavaProject\target\classes' 'com.adevguide.java.problems.ReverseInteger'

---
+++ SimpleJavaProject/src/main/java/com/adevguide/java/problems/ReverseInteger.java
@@ -0,0 +1,23 @@
+package com.adevguide.java.problems;
+
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        //Variable to hold input value
+        Integer input = 123456;
+        //Variable to hold reversed value
+        Integer reverse = 0;
+        //loop till input value becomes zero
+        while (input != 0) {
+            //variable to store ramainder value
+            Integer remainder = input % 10;
+            //multiple previous reverse value by 10 , add remainder to it and save it back in reverse variable
+            reverse = reverse * 10 + remainder;
+            //Divide input by 10 and store quotient value in input
+            input = input / 10;
+        }
+        //Print reverse Value
+        System.out.println("Reverse Integer ::" + reverse);
+    }
+
+}
